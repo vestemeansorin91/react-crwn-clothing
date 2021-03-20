93a7e83de7b503b4414d1b1a7d7d335a16408b43
Sorin Vestemean <serban.vatavu@softwareandpeople.ro>
2021-03-20 22:35:03 +0200

    project arhitecture

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.component.scss';
+import Directory from '../../components/directory/directory.component';
+
+const Homepage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default Homepage;
