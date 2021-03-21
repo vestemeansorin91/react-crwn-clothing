21611d1808e145e9ea58eaa84474ee8fc8b9d932
Sorin Vestemean <serban.vatavu@softwareandpeople.ro>
2021-03-21 15:21:36 +0200

    sign in added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx';
+import AuthPage from './pages/auth/auth.component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={AuthPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/auth/auth.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './auth.component.scss';
+
+const AuthPage = () => (
+  <div className='auth'>
+    <SignIn />
+  </div>
+);
+
+export default AuthPage;
