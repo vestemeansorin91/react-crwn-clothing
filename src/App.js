2b0f8550acb3a6f7ad4a841349ee5af0a8b52e22
Sorin Vestemean <serban.vatavu@softwareandpeople.ro>
2021-03-21 12:54:35 +0200

    collection preview + item added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import { Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Route exact='true' path='/' component={Homepage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
